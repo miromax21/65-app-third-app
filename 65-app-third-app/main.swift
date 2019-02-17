5049c7a40669d430deae2f9af5703dc8cd089d5b
miroma21 <miromax21@gmail.com>
2019-02-17 14:43:43 +0400

    refactoring

--- 65-app-third-app/main.swift
+++ 65-app-third-app/main.swift
@@ -5,50 +5,64 @@
 //  Created by Princess Max on 11.02.2019.
 //  Copyright © 2019 Princess Max. All rights reserved.
 //
-
 import Foundation
 import Alamofire
 import SwiftyJSON
 
-public struct GithubUser {
+enum AppConstant {
+    static let githubURL = "https://api.github.com"
+}
+//model received from the server with all the parameters
+public struct GithubUserProjectData {
+    public let projectName : String
+    public let projectId : Int
+    //other params
+}
+//model with only custom parameters to view
+public struct Project {
     public let projectName : String
 }
 protocol JSONDecdable {
     init?(JSON:Any)
 }
-extension GithubUser : JSONDecdable{
+extension GithubUserProjectData : JSONDecdable{
    public init?(JSON: Any) {
         guard let JSON = JSON as? [String:Any] else { return nil}
         guard let project_name = JSON["full_name"] as? String else {return nil}
+        guard let project_id = JSON["id"] as? Int else {return nil}
         self.projectName = project_name
+        self.projectId = project_id
     }
 }
 class DataProvider{
-    var githubUsers = [GithubUser]()
-    private func getUserAllRepositoryData(url:String, completion: @escaping ([GithubUser]) -> ()){
+    var githubUsers = [GithubUserProjectData]()
+    private func getUserAllRepositoryData(url:String, completion: @escaping ([GithubUserProjectData]) -> ()){
         Alamofire.request(url).responseJSON { (responseData) -> Void in
             if responseData.result.isFailure {
                 print(responseData.result.error?.localizedDescription ?? "for some reason, data could not be obtained")
-                completion(self.githubUsers)
             }
             if((responseData.result.value) != nil) {
                 guard let swiftyJsonArray = responseData.result.value as? [[String : Any]] else {return}
                 for swiftyJson in swiftyJsonArray{
-                    guard let user = GithubUser(JSON: swiftyJson) else { return }
+                    guard let user = GithubUserProjectData(JSON: swiftyJson) else { return }
                     self.githubUsers.append(user)
                 }
                 completion(self.githubUsers)
             }
         }
     }
-    func getUserDataAsync(url:String, completion: @escaping ([GithubUser]) -> ()){
+    func getUserProjectsInfoAsync(url:String, completion: @escaping ([Project]) -> ()){
         let dataLoaderQueue = DispatchQueue.global(qos: .background)
+        var projects = [Project]()
         dataLoaderQueue.async {
-            self.getUserAllRepositoryData(url: url, completion: { [weak self] (users: [GithubUser]) in
+            self.getUserAllRepositoryData(url: url, completion: { [weak self] (users: [GithubUserProjectData]) in
                 guard let self = self else { return }
                 self.githubUsers = users
+                for project in self.githubUsers{
+                    projects.append(Project(projectName: project.projectName))
+                }
                 DispatchQueue.main.async {
-                    completion(self.githubUsers)
+                    completion(projects)
                 }
             })
         }
@@ -56,19 +70,18 @@ class DataProvider{
 }
 
 class App {
-    var users: [GithubUser] = []
-    func run(username: String){
-        let url = "https://api.github.com/users/\(username)/repos"
-        DataProvider().getUserDataAsync(url: url) { (users:[GithubUser]) in
-            for user in users{
-              print(user.projectName)
+    func getTheUserProjectsInfo(username: String){
+        let url = "\(AppConstant.githubURL)/users/\(username)/repos"
+        DataProvider().getUserProjectsInfoAsync(url: url) { (projects:[Project]) in
+            for project in projects{
+              print(project.projectName)
             }
         }
     }
 }
-print("enter user name")
+print("enter the user name")
 if let username = readLine(){
-    App().run(username: username)
+    App().getTheUserProjectsInfo(username: username)
 }
 RunLoop.main.run()
 
