a5077258dbddcddc29fe0f9652ec8bc91064856d
miroma21 <miromax21@gmail.com>
2019-02-23 14:56:04 +0400

    *code review

--- 65-app-third-app/main.swift
+++ 65-app-third-app/main.swift
@@ -12,15 +12,13 @@ import SwiftyJSON
 enum AppConstant {
     static let githubURL = "https://api.github.com"
 }
-//model received from the server with all the parameters
 public struct GithubUserProjectData {
     public let projectName : String
     public let projectId : Int
-    //other params
 }
-//model with only custom parameters to view
-public struct Project {
-    public let projectName : String
+
+public class Datas {
+    static var projects = [GithubUserProjectData]()
 }
 protocol JSONDecdable {
     init?(JSON:Any)
@@ -35,36 +33,25 @@ extension GithubUserProjectData : JSONDecdable{
     }
 }
 class DataProvider{
-    var githubUsers = [GithubUserProjectData]()
-    private func getUserAllRepositoryData(url:String, completion: @escaping ([GithubUserProjectData]) -> ()){
-        Alamofire.request(url).responseJSON { (responseData) -> Void in
-            if responseData.result.isFailure {
-                print(responseData.result.error?.localizedDescription ?? "for some reason, data could not be obtained")
+    static func getUserAllRepositoryData(url:String, completion: @escaping () -> ()){
+        var githubUsers = [GithubUserProjectData]()
+        DispatchQueue.global(qos: .userInteractive).async{
+            Alamofire.request(url).responseJSON { (responseData) -> Void in
+                if responseData.result.isFailure {
+                    print(responseData.result.error?.localizedDescription ?? "for some reason, data could not be obtained")
+                }
+                if((responseData.result.value) != nil) {
+                    guard let swiftyJsonArray = responseData.result.value as? [[String : Any]] else {return}
+                    for swiftyJson in swiftyJsonArray{
+                        guard let user = GithubUserProjectData(JSON: swiftyJson) else { return }
+                        githubUsers.append(user)
+                    }
+                    Datas.projects = githubUsers
+                    DispatchQueue.main.async {
+                        completion()
+                    }
+                }
             }
-            if((responseData.result.value) != nil) {
-                guard let swiftyJsonArray = responseData.result.value as? [[String : Any]] else {return}
-                for swiftyJson in swiftyJsonArray{
-                    guard let user = GithubUserProjectData(JSON: swiftyJson) else { return }
-                    self.githubUsers.append(user)
-                }
-                completion(self.githubUsers)
-            }
-        }
-    }
-    func getUserProjectsInfoAsync(url:String, completion: @escaping ([Project]) -> ()){
-        let dataLoaderQueue = DispatchQueue.global(qos: .background)
-        var projects = [Project]()
-        dataLoaderQueue.async {
-            self.getUserAllRepositoryData(url: url, completion: { [weak self] (users: [GithubUserProjectData]) in
-                guard let self = self else { return }
-                self.githubUsers = users
-                for project in self.githubUsers{
-                    projects.append(Project(projectName: project.projectName))
-                }
-                DispatchQueue.main.async {
-                    completion(projects)
-                }
-            })
         }
     }
 }
@@ -72,8 +59,8 @@ class DataProvider{
 class App {
     func getTheUserProjectsInfo(username: String){
         let url = "\(AppConstant.githubURL)/users/\(username)/repos"
-        DataProvider().getUserProjectsInfoAsync(url: url) { (projects:[Project]) in
-            for project in projects{
+        DataProvider.getUserAllRepositoryData(url: url) {
+            for project in Datas.projects{
               print(project.projectName)
             }
         }
